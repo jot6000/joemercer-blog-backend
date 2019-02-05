bbcf9d5f26126c7cdbd17f90c234fe1212698f47
Joe Mercer <joemercer50@googlemail.com>
2019-02-05 16:34:00 +0000

    Node project setup and first two api routes created for testing.

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+const db = require('./routes')
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Joemercer Blog Server is online' })
+})
+app.get('/projectPreviews', db.getProjectPreviews)
+app.get('/projectPage/:urlpostfix', db.getProjectPage)
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
---
+++ routes.js
@@ -0,0 +1,29 @@
+const pool = require('./pool')
+
+const getProjectPreviews = (request, response) => {
+    pool.connection.query('SELECT title,preview,urlpostfix FROM projects ORDER BY id ASC', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getProjectPage = (request, response) => {
+
+    const urlpostfix = request.params.urlpostfix
+
+    console.log(urlpostfix)
+
+    pool.connection.query('SELECT content FROM projects WHERE urlpostfix = $1 ORDER BY id ASC', [urlpostfix], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows[0])
+    })
+}
+
+module.exports = {
+    getProjectPreviews,
+    getProjectPage,
+}
