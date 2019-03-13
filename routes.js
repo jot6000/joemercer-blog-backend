8b0d90392f796e5823dcceed39ad6d9cde253b2b
Joe Mercer <joemercer50@googlemail.com>
2019-03-13 12:25:19 +0000

    Updated addProject to match working add post.

--- routes.js
+++ routes.js
@@ -48,22 +48,30 @@ const getPostPage = (request, response) => {
 
 const addProject = (request, response) => {
 
-    const title = request.params.title
-    const preview = request.params.preview
-    const urlpostfix = request.params.urlpostfix
-    const content = request.params.content
+    //Just evaluate this as true for testing purposes and switch it back when publishing
+    //process.env.PASSPHRASE == request.body.passphrase
+    if(process.env.PASSPHRASE == request.body.passphrase){
+        const title = request.body.title
+        const preview = request.body.preview
+        const urlpostfix = request.body.urlpostfix
+        const content = request.body.content
 
-    pool.connection.query('INSERT INTO projects (title,preview,urlpostfix,content) VALUES ($1,$2,$3,$4)', [title, preview, urlpostfix, content], (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200)
-    })
+        pool.connection.query('INSERT INTO projects (title,preview,urlpostfix,content) VALUES ($1,$2,$3,$4)', [title, preview, urlpostfix, content], (error, results) => {
+            if (error) {
+                response.status(500).json(error)
+            }
+            response.status(200).json({ succsess: 'We did it' })
+        })
+    }
+    else{
+        response.status(500).json({error:'passphrase incorrect or not provided'})
+    }
 }
 
 const addPost = (request, response) => {
 
     //Just evaluate this as true for testing purposes and switch it back when publishing
+    //process.env.PASSPHRASE == request.body.passphrase
     if(process.env.PASSPHRASE == request.body.passphrase){
         const title = request.body.title
         const preview = request.body.preview
